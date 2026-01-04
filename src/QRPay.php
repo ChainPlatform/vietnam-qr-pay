3fbfa58bd1073d849ad8dac0020fcd906a10ec8b
ChainPlatform <support@chainplatform.net>
2026-01-04 15:25:51 +0700

    Update QRPay.php

--- src/QRPay.php
+++ src/QRPay.php
@@ -5,22 +5,30 @@ namespace ChainPlatform\Pay;
 use ChainPlatform\Pay\Constants\FieldID;
 use ChainPlatform\Pay\Constants\QRProviderGUID;
 use ChainPlatform\Pay\Constants\VietQRService;
+use ChainPlatform\Pay\Constants\ProviderFieldID;
+use ChainPlatform\Pay\Constants\VietQRConsumerFieldID;
+use ChainPlatform\Pay\Constants\AdditionalDataID;
 
 class QRPay
 {
     public $isValid = true;
-    public $version = '01';
-    public $initMethod = '11';
+    public $version;
+    public $initMethod;
     public $provider;
+    public $merchant;
     public $consumer;
     public $amount;
+    public $currency = '704';
+    public $nation = 'VN';
     public $additionalData;
+    public $crc;
     public $EVMCo = [];
     public $unreserved = [];
 
     public function __construct(string $content = '')
     {
         $this->provider = new \stdClass();
+        $this->merchant = new \stdClass();
         $this->consumer = new \stdClass();
         $this->additionalData = new \stdClass();
         if (!empty($content)) {
@@ -37,17 +45,136 @@ class QRPay
         $this->parseRootContent($content);
     }
 
+    private function parseRootContent(string $content): void
+    {
+        if (strlen($content) < 4) {
+            return;
+        }
+
+        $slice = $this->sliceContent($content);
+        $id = $slice['id'];
+        $value = $slice['value'];
+        $nextValue = $slice['nextValue'];
+
+        switch ($id) {
+            case FieldID::VERSION: $this->version = $value;
+                break;
+            case FieldID::INIT_METHOD: $this->initMethod = $value;
+                break;
+            case FieldID::VIETQR:
+            case FieldID::VNPAYQR:
+                $this->provider->fieldId = $id;
+                $this->parseProviderInfo($value);
+                break;
+            case FieldID::AMOUNT: $this->amount = $value;
+                break;
+            case FieldID::CURRENCY: $this->currency = $value;
+                break;
+            case FieldID::NATION: $this->nation = $value;
+                break;
+            case FieldID::MERCHANT_NAME: $this->merchant->name = $value;
+                break;
+            case FieldID::ADDITIONAL_DATA: $this->parseAdditionalData($value);
+                break;
+            case FieldID::CRC: $this->crc = $value;
+                break;
+            default:
+                $idNum = (int)$id;
+                if ($idNum >= 65 && $idNum <= 79) {
+                    $this->EVMCo[$id] = $value;
+                } elseif ($idNum >= 80 && $idNum <= 99) {
+                    $this->unreserved[$id] = $value;
+                }
+                break;
+        }
+        $this->parseRootContent($nextValue);
+    }
+
+    private function parseProviderInfo(string $content): void
+    {
+        if (strlen($content) < 4) {
+            return;
+        }
+
+        $slice = $this->sliceContent($content);
+        $id = $slice['id'];
+        $value = $slice['value'];
+
+        switch ($id) {
+            case ProviderFieldID::GUID:
+                $this->provider->guid = $value;
+                break;
+            case ProviderFieldID::DATA:
+                if ($this->provider->guid === QRProviderGUID::VIETQR) {
+                    $this->parseVietQRConsumer($value);
+                } elseif ($this->provider->guid === QRProviderGUID::VNPAY) {
+                    $this->merchant->id = $value;
+                }
+                $this->provider->data = $value;
+                break;
+            case ProviderFieldID::SERVICE:
+                $this->provider->service = $value;
+                break;
+        }
+        $this->parseProviderInfo($slice['nextValue']);
+    }
+
+    private function parseVietQRConsumer(string $content): void
+    {
+        if (strlen($content) < 4) {
+            return;
+        }
+
+        $slice = $this->sliceContent($content);
+        if ($slice['id'] === VietQRConsumerFieldID::BANK_BIN) {
+            $this->consumer->bankBin = $slice['value'];
+        } elseif ($slice['id'] === VietQRConsumerFieldID::BANK_NUMBER) {
+            $this->consumer->bankNumber = $slice['value'];
+        }
+        $this->parseVietQRConsumer($slice['nextValue']);
+    }
+
+    private function parseAdditionalData(string $content): void
+    {
+        if (strlen($content) < 4) {
+            return;
+        }
+
+        $slice = $this->sliceContent($content);
+        switch ($slice['id']) {
+            case AdditionalDataID::PURPOSE_OF_TRANSACTION: $this->additionalData->purpose = $slice['value'];
+                break;
+            case AdditionalDataID::BILL_NUMBER: $this->additionalData->billNumber = $slice['value'];
+                break;
+            case AdditionalDataID::MOBILE_NUMBER: $this->additionalData->mobileNumber = $slice['value'];
+                break;
+            case AdditionalDataID::STORE_LABEL: $this->additionalData->store = $slice['value'];
+                break;
+            case AdditionalDataID::TERMINAL_LABEL: $this->additionalData->terminal = $slice['value'];
+                break;
+        }
+        $this->parseAdditionalData($slice['nextValue']);
+    }
+
     public function build(): string
     {
-        $content = self::genFieldData(FieldID::VERSION, $this->version);
-        $content .= self::genFieldData(FieldID::INIT_METHOD, $this->initMethod);
+        $content = self::genFieldData(FieldID::VERSION, $this->version ?? '01');
+        $content .= self::genFieldData(FieldID::INIT_METHOD, $this->initMethod ?? '11');
 
-        $innerData = self::genFieldData('00', $this->consumer->bankBin) . self::genFieldData('01', $this->consumer->bankNumber);
-        $providerData = self::genFieldData('00', $this->provider->guid) . self::genFieldData('01', $innerData) . self::genFieldData('02', $this->provider->service);
+        $guid = self::genFieldData(ProviderFieldID::GUID, $this->provider->guid);
+        $innerData = self::genFieldData(VietQRConsumerFieldID::BANK_BIN, $this->consumer->bankBin) .
+                     self::genFieldData(VietQRConsumerFieldID::BANK_NUMBER, $this->consumer->bankNumber);
+
+        $providerData = $guid . self::genFieldData(ProviderFieldID::DATA, $innerData) .
+                        self::genFieldData(ProviderFieldID::SERVICE, $this->provider->service);
 
         $content .= self::genFieldData($this->provider->fieldId, $providerData);
+        $content .= self::genFieldData(FieldID::CURRENCY, $this->currency ?? '704');
         $content .= self::genFieldData(FieldID::AMOUNT, $this->amount);
-        $content .= self::genFieldData(FieldID::ADDITIONAL_DATA, self::genFieldData('08', $this->additionalData->purpose));
+        $content .= self::genFieldData(FieldID::NATION, $this->nation ?? 'VN');
+
+        $addContent = self::genFieldData(AdditionalDataID::PURPOSE_OF_TRANSACTION, $this->additionalData->purpose);
+        $content .= self::genFieldData(FieldID::ADDITIONAL_DATA, $addContent);
 
         ksort($this->EVMCo);
         foreach ($this->EVMCo as $id => $val) {
@@ -65,6 +192,7 @@ class QRPay
     public static function initVietQR(array $options): self
     {
         $qr = new self();
+        $qr->initMethod = isset($options['amount']) ? '12' : '11';
         $qr->provider->fieldId = FieldID::VIETQR;
         $qr->provider->guid = QRProviderGUID::VIETQR;
         $qr->provider->service = $options['service'] ?? VietQRService::BY_ACCOUNT_NUMBER;
@@ -75,38 +203,28 @@ class QRPay
         return $qr;
     }
 
-    public function setEVMCoField(string $id, string $value): void
-    {
-        if ((int)$id >= 65 && (int)$id <= 79) {
-            $this->EVMCo[$id] = $value;
-        }
-    }
-    public function setUnreservedField(string $id, string $value): void
-    {
-        if ((int)$id >= 80 && (int)$id <= 99) {
-            $this->unreserved[$id] = $value;
-        }
-    }
     private function verifyCRC($c): bool
     {
-        return strtoupper(substr($c, -4)) === strtoupper(str_pad(dechex(CRC16::crc16ccitt(substr($c, 0, -4))), 4, '0', STR_PAD_LEFT));
+        $check = substr($c, 0, -4);
+        $crcCode = strtoupper(substr($c, -4));
+        return $crcCode === strtoupper(str_pad(dechex(CRC16::crc16ccitt($check)), 4, '0', STR_PAD_LEFT));
     }
+
     private function sliceContent($c): array
     {
-        return ['id' => substr($c, 0, 2), 'value' => substr($c, 4, (int)substr($c, 2, 2)), 'nextValue' => substr($c, 4 + (int)substr($c, 2, 2))];
+        $id = substr($c, 0, 2);
+        $lenString = substr($c, 2, 2);
+        $length = (int)$lenString;
+        $value = substr($c, 4, $length);
+        $nextValue = substr($c, 4 + $length);
+        return ['id' => $id, 'value' => $value, 'nextValue' => $nextValue];
     }
+
     private static function genFieldData($i, $v): string
     {
-        return (empty($i) || empty($v)) ? '' : $i . str_pad(strlen($v), 2, '0', STR_PAD_LEFT) . $v;
-    }
-    private function parseRootContent($c): void
-    {
-        $slice = $this->sliceContent($c);
-        if ($slice['id'] === FieldID::VERSION) {
-            $this->version = $slice['value'];
+        if (empty($i) || $v === null || $v === '') {
+            return '';
         }
-        if (strlen($slice['nextValue']) > 4) {
-            $this->parseRootContent($slice['nextValue']);
-        }
+        return $i . str_pad(strlen($v), 2, '0', STR_PAD_LEFT) . $v;
     }
 }
